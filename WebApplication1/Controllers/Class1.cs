417b9c6733a1efe3889626bee66805db48bbfd24
Jushun Blackmon <Jushun.Blackmon@hotmail.com>
2018-10-15 15:19:18 -0500

    Added Detail action method

--- WebApplication1/Controllers/Class1.cs
+++ WebApplication1/Controllers/Class1.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public string Detail()
         {
-            return "Hello from  the Hoop capital USA";
+            return "Hello from  the Hoop capital USA!";
         }
     }
 }
