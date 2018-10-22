ddc0e933ebfd1dabb14704254c7fa5aac452d9b5
Jushun Blackmon <Jushun.Blackmon@hotmail.com>
2018-10-22 10:03:42 -0500

    Updated Comic Book Detail view

--- WebApplication1/Controllers/Class1.cs
+++ WebApplication1/Controllers/Class1.cs
@@ -10,6 +10,18 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
+           ViewBag.SeriesTitle = "The Man of Steel";
+           ViewBag.IssueNumber = 700;
+           ViewBag.Description = "<p>Final issue! Witness the final hours of Lex Luthors' life and his one, last, great act of revenge! Even if Superman survives... <strong>will Clark Kent?</strong></p>";
+           ViewBag.Artists = new string[]
+            {
+                "Script: Jushun Blackmon",
+                "Pencils: Ceaser Drawer",
+                "Inks: Manny Ink",
+                "Colors: Johnny Rambo",
+                "Letters: Lucy Letter"
+            };
+
             return View();
         }
     }
