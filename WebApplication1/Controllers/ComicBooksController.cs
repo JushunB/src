fd3cf4db0edf820827a5cf2ebfd8da6937d0a26a
Jushun Blackmon <Jushun.Blackmon@hotmail.com>
2018-11-19 14:10:39 -0600

    Updated Comic Book Detail view to be strongly typed

--- WebApplication1/Controllers/ComicBooksController.cs
+++ WebApplication1/Controllers/ComicBooksController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -10,19 +11,22 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Detail()
         {
-           ViewBag.SeriesTitle = "The Man of Steel";
-           ViewBag.IssueNumber = 700;
-           ViewBag.Description = "<p>Final issue! Witness the final hours of Lex Luthors' life and his one, last, great act of revenge! Even if Superman survives... <strong>will Clark Kent?</strong></p>";
-           ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Jushun Blackmon",
-                "Pencils: Ceaser Drawer",
-                "Inks: Manny Ink",
-                "Colors: Johnny Rambo",
-                "Letters: Lucy Letter"
+                SeriesTitle = "The Man of Steel",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Lex Luthors' life and his one, last, great act of revenge! Even if Superman survives... <strong>will Clark Kent?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Jushun Blackmon", Role = "Script"},
+                    new Artist() {Name = "Ceaser Drawer", Role = "Pencils"},
+                    new Artist() {Name = "Manny Ink", Role = "Inks"},
+                    new Artist() {Name = "Johnny Rambo", Role = "Colors"},
+                    new Artist() {Name = "Lucy Letter", Role = "Letters"},
+                }
             };
 
-            return View();
+            return View(comicBook);
         }
     }
 }
