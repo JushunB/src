69866180ed103a645021be56e572ba0845e55131
Jushun Blackmon <Jushun.Blackmon@hotmail.com>
2018-11-20 10:21:26 -0600

    Added Comic Book Repository

---
+++ WebApplication1/Data/ComicBookRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                SeriesTitle = "The Man of Steel",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Lex Luthors' life and his one, last, great act of revenge! Even if Superman survives... <strong>will Clark Kent?</strong></p>",
+                Artists = new[]
+                {
+                    new Artist() {Name = "Jushun Blackmon", Role = "Script"},
+                    new Artist() {Name = "Ceaser Drawer", Role = "Pencils"},
+                    new Artist() {Name = "Manny Ink", Role = "Inks"},
+                    new Artist() {Name = "Johnny Rambo", Role = "Colors"},
+                    new Artist() {Name = "Lucy Letter", Role = "Letters"},
+                }
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" },
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                },
+                Favorite = false
+            }
+    };
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+
+                    break;
+                }
+            }
+
+            return comicBookToReturn;
+        }
+    }
+}
